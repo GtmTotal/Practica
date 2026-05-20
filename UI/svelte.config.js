1676e94a5053b8855326189b0d3ba87a8cd78121
APH <alepenalva2@gmail.com>
2026-05-20 11:58:36 +0200

    Fix Netlify deployment: use adapter-static and correct build config

--- UI/svelte.config.js
+++ UI/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +11,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-    out: 'build'
-})
+			fallback: 'index.html'
+		})
 	}
 };
 
